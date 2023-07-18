4e86f0daa50325e2b87a6a43ecd20a039f23f974
sachin23 <116019893+sachin443@users.noreply.github.com>
2023-07-18 10:45:23 +0530

    Update script.js
    change number

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const callButton = document.getElementById('callButton');
 
 // Replace the phone number with the actual phone number you want to call
-const phoneNumber = '8952977436';
+const phoneNumber = '6378522871';
 
 callButton.addEventListener('click', function() {
     // Use the tel: protocol to initiate a phone call
